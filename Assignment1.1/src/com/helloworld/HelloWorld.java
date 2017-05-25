ee3b64dc43fefc84a36fcbb86d40025eb3d75add
Sunil <sunilpdt@gmail.com>
2017-05-25 12:07:05 +0530

    Changes committed in local repo

---
+++ Assignment1.1/src/com/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HelloWorld");
+
+	}
+
+}
